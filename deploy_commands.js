a3063f52a65ff750c92c673fae5a46d0d6a1cc45
Parker Thornton <pathornton114@gmail.com>
2022-09-12 20:46:39 -0500

    Update

--- deploy_commands.js
+++ deploy_commands.js
@@ -3,7 +3,8 @@ const { REST } = require('@discordjs/rest');
 const { clientId, token } = require('./config.json');
 
 const commands = [
-	new SlashCommandBuilder().setName('help').setDescription('Replies with pong!')
+	new SlashCommandBuilder().setName('help').setDescription('Replies with pong!'),
+	new SlashCommandBuilder().setName('test').setDescription('Test Command')
 ]
 	.map(command => command.toJSON());
 
--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.json');
+const { token, access_token, pageId, announcementChannel } = require('./config.json');
 const { helpEmbed } = require("./embeds")
+
+var FB = require('facebook-node');
+FB.setAccessToken(access_token);
+
+
+// Main Facebook loop
+var liveVideo;
+
+// const mainLoop = async () => {
+// 	await FB.api(`${pageId}/live_videos?limit=1`, function (res) {
+// 		console.log(res)
+// 		const video = res.data[0];
+
+// 		if (liveVideo === video.id) return;
+
+// 		if (video.status === "VOD") {
+// 			client.channels.cache.get(announcementChannel).send("Live! " + video.id);
+// 			liveVideo = video.id;
+// 		}
+// 	});
+
+// 	setTimeout(() => mainLoop(), 5000)
+// }
+
+
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -15,6 +40,9 @@ client.on('interactionCreate', async interaction => {
 
 	if (commandName === 'help') {
 		await interaction.reply({embeds: [helpEmbed]});
+	} else if (commandName === "test") {
+		await client.channels.cache.get(announcementChannel).send("Test")
+		await interaction.reply("message Sent")
 	}
 });
 
